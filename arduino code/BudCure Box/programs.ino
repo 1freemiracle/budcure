2af5856f011b4bba53e96b422cd157cecd2d2dd3
1freemiracle <bhahaj@gmail.com>
2024-12-21 20:26:12 -0600

    Update programs.ino

--- arduino code/BudCure Box/programs.ino
+++ arduino code/BudCure Box/programs.ino
@@ -1,28 +1,44 @@
+/*
+this program calculates the dew point and uses the resulting error between measurement and calcuated value to set the pwm for the TEC. A frosty temp is used to adjust to prevent frost buildup. 
+*/
 void tecProgram() {
-  digitalWrite(TEC_PIN, !analogRead(TEC_PIN));
-  delay(500);
-  tareScreen();
-}
+  float celsius = (temperature - 32) * 5.0 / 9.0;
+  if (humidity > humidities && tempC > frosty) {
 
+    float error = tempC - dewPoint;
+    if (error >= 5) {
+      analogWrite(TEC_PIN, 255);
+    } else if (error < 5 && error > 1){
+    pwmTec = map(error, 0, 10, 100, 255);
+    pwmTec = constrain(pwmTec, 0, 255);
+    analogWrite(TEC_PIN, pwmTec);
+    } else {analogWrite(TEC_PIN, 0);}
+  } else {analogWrite(TEC_PIN, 0);}
+
+}
+/*
+This program starts the fan every "fanCycleInterval" (x times in an hour) for length of time "fanOnDuration" (in minutes).
+*/
 void fanProgram() {
-  currentProgram = "FAN";
-  if (firstTimeThrough && humidity > 75) {
-    digitalWrite(FAN_PIN, HIGH);  // Turn on the fan on the first run if humidity is above 75%
-    fanState = true;              // Update the fan state
-    firstTimeThrough = false;     // Mark that the first time through is done
-  } else if (humidity > 77 && !fanState) {
-    digitalWrite(FAN_PIN, HIGH);  // Turn on the fan if humidity exceeds 77%
-    fanState = true;              // Update the fan state
-  } else if (humidity < 75 && fanState) {
-    digitalWrite(FAN_PIN, LOW);   // Turn off the fan if humidity falls below 75%
-    fanState = false;             // Update the fan state
+  // Calculate the interval between fan cycles in minutes
+  fanCycleInterval = 60 / fanCyclesPerHour;
+
+  if ((minutes - fanStartMinute) >= fanCycleInterval && !fanState) {
+    // Time to turn on the fan
+    digitalWrite(FAN_PIN, HIGH);
+    fanState = true;
+    fanStartMinute = minutes;  // Record the minute the fan was turned on
+  }
+
+  // Turn off the fan after the specified duration (in minutes)
+  if (fanState && (minutes - fanStartMinute >= fanOnDuration)) {
+    digitalWrite(FAN_PIN, LOW);
+    fanState = false;
   }
 }
 
-
-
 void resetMemory() {
-  for (int i = 20; i < 512; i++) {
+  for (int i = 30; i < 512; i++) {
         EEPROM.write(i, 0); 
     }
     
@@ -30,10 +46,12 @@ void resetMemory() {
     EEPROM.commit();
     eventCount = 0;
 
-    for (int i = 0; i < 100; i++) {
+    for (int i = 0; i < 99; i++) {
       weights[i] = 0;
     }
-
+    SPIFFS.remove("/data.csv");
+    SPIFFS.remove("/data2.csv");
+    delay(500);
     r.resetPosition(1);
     position = 1;
 }
